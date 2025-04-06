c92e907a59ee45d7ac3093735943cc7b7bd3b4e1
Edwin <enginebox2012@gmail.com>
2025-04-06 22:16:21 +0200

    Initial setup a nestjs project.

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { UniversityModule } from './university/university.module';
+import { LoggerService } from './common/logger.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
+    UniversityModule,
+  ],
+  providers: [LoggerService],
+})
+export class AppModule {} 
